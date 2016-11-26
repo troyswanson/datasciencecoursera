9480c17791db1a1bdbb9b7ff323d7dcf4b6014af
Troy Swanson <gerphimum@gmail.com>
2016-11-26 11:24:18 -0600

    Optimize corr function

--- 02-r-programming/wk2/assignment-1/corr.R
+++ 02-r-programming/wk2/assignment-1/corr.R
@@ -1,16 +1,12 @@
 corr <- function(directory, threshold = 0) {
-  l <- list()
+  x <- numeric()
   for(i in list.files(directory)) {
     path <- sprintf("%s/%s", directory, i)
     data <- read.csv(path)
     obs <- data[complete.cases(data), ]
     if(nrow(obs) > threshold) {
-      l[[i]] <- obs
+      x <- append(x, cor(obs$sulfate, obs$nitrate))
     }
-  }
-  x <- numeric()
-  for(i in l) {
-    x <- append(x, cor(i$sulfate, i$nitrate))
   }
   x
 }
