b7ecd93b564f7252e92c830861fcc3f3709fb46d
Troy Swanson <gerphimum@gmail.com>
2017-01-01 22:47:04 -0600

    Update script to be complete

--- 03-getting-cleaning-data/assignment-1/run_analysis.R
+++ 03-getting-cleaning-data/assignment-1/run_analysis.R
@@ -1,3 +1,5 @@
+library(plyr)
+
 # returns a character vector of features that are present in each row in the data set
 getFeatures <- function () {
   path <- "./data/features.txt"
@@ -6,7 +8,14 @@ getFeatures <- function () {
   
   x <- read.table(path, sep = " ", col.names = names, colClasses = classes)
   
-  x$feature
+  f <- x$feature
+  
+  # clean up names
+  f <- gsub("[/(/)]", "", f)
+  f <- gsub(",", "-", f)
+  f <- sub("BodyBody", "Body", f)
+  
+  f
 }
 
 # returns a data frame of labels to classify activities
@@ -33,6 +42,17 @@ getActivities <- function (set) {
   x
 }
 
+# returns a numeric vector of subjects for each row in the given data set
+getSubjects <- function (set) {
+  path <- sprintf("./data/%s/subject_%s.txt", set, set)
+  names <- c("subject")
+  classes <- c("numeric")
+  
+  x <- read.table(path, sep = " ", col.names = names, colClasses = classes)
+  
+  x$subject
+}
+
 # returns a data frame for the given data set, including the row's activity
 getSet <- function (set) {
   path <- sprintf("./data/%s/X_%s.txt", set, set)
@@ -40,22 +60,30 @@ getSet <- function (set) {
   widths <- rep.int(16, length(features))
   
   x <- read.fwf(path, widths, col.names = features, colClasses = "numeric", buffersize = 50)
+  
   x$activity <- getActivities(set)
+  x$subject <- getSubjects(set)
   
   x
 }
 
+
+# BEGIN SCRIPT #
+
 # get both datasets and bind them to a single data frame
 d <- rbind(getSet("test"), getSet("train"))
 
-# subset the data frame to only show columns containing "mean", "std", and "activity"
-d <- d[, grep("([Mm]ean|std|activity)", names(d))]
+# convert the subject column into a factor
+d$subject <- factor(d$subject)
 
-# split the data frame into a list of data frames based on activity
-s <- split(d, d$activity)
+# subset the data frame to only show columns containing "mean", "std", "activity", and "subject"
+d <- d[, grep("([Mm]ean|std|activity|subject)", names(d))]
 
-# get the column averages for each item in the list
-averages <- lapply(s, function(x) colMeans(x[, grep("[^activity]", names(x))]))
+# get aggregate averages across subject and activity
+averages <- aggregate(. ~ subject + activity, d, mean)
 
-# create a data frame of the averages for tidiness
-averages <- as.data.frame(do.call(rbind, averages))
+# order the data by subject and activity for cleanliness
+averages <- averages[order(averages$subject, averages$activity), ]
+
+# write out the result to a text file
+write.table(averages, file = "averages.txt", row.name = FALSE)
